570c49528f0f1fd9f9f5d95a92c2d0b0846b7dfb
nicolas-meilan <sebastian.nicolas.meilan@gmail.com>
2024-11-29 04:11:07 -0300

    change wordings

--- src/configuration/chat.ts
+++ src/configuration/chat.ts
@@ -1,15 +1,23 @@
 export const MESSAGES_CONFIG: {
     [message: string]: string;
 } = {
-    ['!redes']: `https://www.twitch.tv/rungekutta93\n
-https://kick.com/rungekutta93`,
-    ['!horarios']: `🎮 **Mi horario de streams** 🎮\n
-🗓️ **Lunes**: 10:30 PM - ¡Empezamos la semana con energía! 🕹️\n
-🗓️ **Martes**: 10:30 PM - ¡Vamos con todo! 🚀\n
-🗓️ **Miércoles**: 10:30 PM - ¡Ya estamos en la mitad! 💪\n
-❌ **Jueves**: Día de descanso para cargar pilas 🧘‍♂️\n
-🗓️ **Viernes**: 10:30 PM - ¡Casi al fin de semana, no hay descanso! 🎉\n
-✨ **Sábado**: ¡A confirmar! Estaré activo en mis redes para avisar 📅\n
-✨ **Domingo**: ¡A confirmar! Estaré activo en mis redes para avisar 📅\n
-¡Nos vemos en el stream! 🚀`
-}
+    ['!redes']: `
+🌐 ¡Sigue mis aventuras en todas las plataformas! 🌐
+
+🎮 **Twitch**: [twitch.tv/rungekutta93](https://www.twitch.tv/rungekutta93)  
+🎮 **Kick**: [kick.com/rungekutta93](https://kick.com/rungekutta93)  
+
+¡Nos vemos en el stream! 🚀✨
+`, ['!horarios']: `
+🎮 **Mi horario de streams** 🎮  
+
+🗓️ **Lunes**: 10:30 PM - ¡Empezamos la semana con energía! 🕹️  
+🗓️ **Martes**: 10:30 PM - ¡Seguimos fuertes! 🚀  
+🗓️ **Miércoles**: 10:30 PM - ¡Mitad de semana, no aflojamos! 💪  
+❌ **Jueves**: Día libre para recargar energías 🧘‍♂️  
+🗓️ **Viernes**: ¡A confirmar! Mantente atento a mis redes 📅  
+🗓️ **Sábado**: ¡A confirmar! Te avisaré por Twitch o Kick ✨  
+🗓️ **Domingo**: 10:30 PM - ¡Cerramos la semana jugando a tope! 🎉  
+
+¡Nos vemos en el stream! 🚀
+`};
