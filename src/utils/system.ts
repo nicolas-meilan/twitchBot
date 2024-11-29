a62ab6375082dfa3c158e7b0f9eca66fe69783aa
Nicolas Meilan <sebastian.nicolas.meilan@gmail.com>
2024-11-29 13:45:57 -0300

    Fix for windows

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,25 +1,25 @@
 import { exec } from 'child_process';
 
 export const openBrowser = (url: string) => {
-    const platform = process.platform;
-    
-    let command: string;
-    
-    if (platform === 'win32') {
-      command = `start "${url}"`;
-    } else if (platform === 'darwin') {
-      command = `open "${url}"`;
-    } else {
-      command = `xdg-open "${url}"`;
-    }
+  const platform = process.platform;
   
-    exec(command, (error, stderr) => {
-      if (error) {
-        throw error;
-      }
-      if (stderr) {
-        throw new Error(stderr);
-      }
-    });
-  };
+  let command: string;
+  
+  if (platform === 'win32') {
+    command = `start "" "${url}"`;
+  } else if (platform === 'darwin') {
+    command = `open "${url}"`;
+  } else {
+    command = `xdg-open "${url}"`;
+  }
+
+  exec(command, (error, stderr) => {
+    if (error) {
+      throw error;
+    }
+    if (stderr) {
+      throw new Error(stderr);
+    }
+  });
+};
   
