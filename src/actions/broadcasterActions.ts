f5c8d688a51692014081fde43b402aaea603032c
Nicolas Meilan <sebastian.nicolas.meilan@gmail.com>
2025-01-04 17:35:22 -0300

    Fix online final message

--- src/actions/broadcasterActions.ts
+++ src/actions/broadcasterActions.ts
@@ -47,8 +47,6 @@ const BROADCASTER_ACTIONS: {
 
     await delay(timeToStartMin * 60 * 1000 * 0.8); // 80% of min to ms
 
-    if (Stream.shared.isOnline) return;
-
     chat.say(BROADCAST_USERNAME, STREAM_START_ALERT_LONG);
     logger.info(STREAM_START_ALERT_LONG);
   },
